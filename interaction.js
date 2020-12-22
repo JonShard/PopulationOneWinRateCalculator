4a7b58901114bc558a5c2d99c6e7bed3c7391e2c
JonShard <jonemskaara@gmail.com>
2020-12-22 14:29:55 +0100

    Wins and losses now stored in a cookie #1

--- interaction.js
+++ interaction.js
@@ -1,19 +1,23 @@
 function init() {
-    document.getElementById("txt_played").value = getCookieValue("played")
-    document.getElementById("txt_wins").value = getCookieValue("wins")
-    document.getElementById("rng_target_win_rate").value = (getCookieValue("targetWinRate") * 100)
-    if (getCookieValue("returningUser") == "true") {
-        caclulateStats()
+    document.getElementById("txt_played").value = getCookie("played")
+    document.getElementById("txt_wins").value = getCookie("wins")
+    document.getElementById("rng_target_win_rate").value = (getCookie("targetWinRate") * 100)
+    if (getCookie("returningUser") == "true") {
+        updateStats()
     }
     else {
-        document.cookie = "consecutiveWins=0; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+        setCookie("consecutiveWins", 0)
         alert("We use cookies to store your games played, games won and target win rate for your convenience. By using this service you agree to cookies.")
   }
 }
 
-function getCookieValue(a) {
-    var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
+function getCookie(key) {
+    var b = document.cookie.match('(^|;)\\s*' + key + '\\s*=\\s*([^;]+)');
     return b ? b.pop() : '';
+}
+
+function setCookie(key, value) {
+    document.cookie = key + "=" + value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=Lax";
 }
 
 function setDisplayMode(className, displayState){
@@ -25,21 +29,31 @@ function setDisplayMode(className, displayState){
 
 function updateTargetWinRateRange() {
   document.getElementById("lbl_rng_target_win_rate").innerHTML = document.getElementById("rng_target_win_rate").value
-  caclulateStats()
+  updateStats()
 }
 
 function addWin(){
   document.getElementById("txt_played").value++
   document.getElementById("txt_wins").value++
-  var consWins = getCookieValue("consecutiveWins")
-  document.cookie = "consecutiveWins=" + (+consWins + 1) + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-  //registerMatchResult("w")
-  caclulateStats()
+  var consWins = getCookie("consecutiveWins")
+  setCookie("consecutiveWins", +consWins + 1)
+  registerMatchResult("w")
+  updateStats()
 }
 
 function addLoss(){
   document.getElementById("txt_played").value++
-  document.cookie = "consecutiveWins=0; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-  //registerMatchResult("l")
-  caclulateStats()
+  setCookie("consecutiveWins", 0)
+  registerMatchResult("l")
+  updateStats()
+}
+
+function registerMatchResult(result) {
+    var history = getCookie("matchHistory")
+    history = history + result + ","
+    setCookie("matchHistory", history) 
+}
+
+function resetMatchHistory() {
+    setCookie("matchHistory", "")
 }
--- stats.js
+++ stats.js
@@ -9,28 +9,39 @@ function getWinsToReachTarget(gamesPlayed, gamesWon, targetWinRate) {
     return (hypPlayed - gamesPlayed)
 }
 
-    function caclulateStats() {
+function updateStats() {
     var gamesPlayed = document.getElementById("txt_played").value
     var gamesWon = document.getElementById("txt_wins").value
     var targetWinRate = document.getElementById("rng_target_win_rate").value / 100
-    document.cookie = "played=" + gamesPlayed + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-    document.cookie = "wins=" + gamesWon + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-    document.cookie = "targetWinRate=" + targetWinRate + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-    document.cookie = "returningUser=" + true + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    setCookie("played", gamesPlayed)
+    setCookie("wins", gamesWon)
+    setCookie("targetWinRate", targetWinRate)
+    setCookie("returningUser", true)
 
     document.getElementById("lbl_games_played").innerHTML = "Games played: " + gamesPlayed
     document.getElementById("lbl_games_won").innerHTML = "Games won: " + gamesWon
     document.getElementById("lbl_games_lost").innerHTML = "Games lost: " + (gamesPlayed - gamesWon)
-    document.getElementById("lbl_consecutive_wins").innerHTML = "Consecutive wins: " + getCookieValue("consecutiveWins");
+    document.getElementById("lbl_consecutive_wins").innerHTML = "Consecutive wins: " + getCookie("consecutiveWins");
     document.getElementById("lbl_wins_at_stake").innerHTML = "Wins at stake: " + (getWinsToReachTarget(+gamesPlayed + 1, gamesWon, targetWinRate) - getWinsToReachTarget(gamesPlayed, gamesWon, targetWinRate))
     document.getElementById("lbl_win_rate").innerHTML = "Win rate: " + ((gamesWon / gamesPlayed) * 100).toFixed(3) + "%"
     document.getElementById("lbl_rounded_win_rate").innerHTML = "Population One rounded win rate: " + ((gamesWon / gamesPlayed).toFixed(2) * 100) + "%"
     document.getElementById("lbl_games_to_target").innerHTML = "Consecutive games to reach goal: " + (getWinsToReachTarget(gamesPlayed, gamesWon, targetWinRate))
     setDisplayMode("stats", "block")
+    tableCreate()
 }
 
-// function registerMatchResult(result) {
-//     var history = getCookieValue("matchHistory")
-//     history += result + ","
-//     document.cookie = "matchHistory=" + history + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";   
+// https://stackoverflow.com/questions/14643617/create-table-using-javascript
+// function tableCreate(){
+//     var parent = document.getElementById("historySection");
+//     // parent.removeChild(0)
+//     tbl  = document.createElement('table');
+//     tbl.style.width  = '100px';
+//     tbl.style.border = '1px solid black';
+//     var tr = tbl.insertRow();
+//     for(var j = 0; j < 2; j++){
+//         var td = tr.insertCell();
+//         td.appendChild(document.createTextNode('Cell'));
+//         td.style.border = '1px solid black';
+//     }
+//     parent.appendChild(table);
 // }
